386fdff37aff74e8620716e164776d8504f11360
langrenzhou <203666984LZDJ1.2.3>
2020-09-20 20:13:04 +0800

    详情页面的天机

--- myapp/src/pages/index/index.tsx
+++ myapp/src/pages/index/index.tsx
@@ -22,7 +22,7 @@ export default class Index extends Component {
    * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
    */
   config: Config = {
-    navigationBarTitleText: '首页'
+    navigationBarTitleText: '比克优选'
   }
 
   render () {
