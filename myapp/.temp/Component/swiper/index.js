37ab362844dd8d99c6f9d3f2b2afaf8d1e6d4fc4
langrenzhou <203666984LZDJ1.2.3>
2020-10-12 21:35:02 +0800

    对于swipeer的完善

--- myapp/.temp/Component/swiper/index.js
+++ myapp/.temp/Component/swiper/index.js
@@ -1,7 +1,9 @@
 import Nerv from "nervjs";
 import Taro from "@tarojs/taro-h5";
 import { View } from '@tarojs/components';
+import { Swiper, SwiperItem, Image } from '@tarojs/components';
 import Ajax from '../../../static/js/Axios';
+import './swiper.css';
 class Swipers extends Taro.Component {
   constructor(props) {
     super(props);
@@ -14,12 +16,18 @@ class Swipers extends Taro.Component {
       this.setState({
         ImgUrlArr: res.data
       });
+      this.render();
     });
   }
   render() {
     return <View>
-             
-                 
+                <View className="SwiperBack">
+                    {this.state.ImgUrlArr.length > 0 ? <Swiper className="test-h" indicatorColor="#fff" indicatorActiveColor="#ec4b6a" interval="1000" circular={this.state.ImgUrlArr.length > 0 ? true : false} indicatorDots autoplay className="Swipers">
+                            {this.state.ImgUrlArr.map(item => <SwiperItem key={item.id} className="SwiperItem">
+                                    <Image src={item.url} style="width:100%;height:100%;"></Image>
+                                </SwiperItem>)}
+                        </Swiper> : ''}
+                </View>
             </View>;
   }
 }
